7aab99b6223b53bf23dc066d1a3222a22352edbe
Yichan Kim <yichankim@gmail.com>
2025-11-25 17:45:52 +0000

    Spacing adjustment for addr.sv

--- repo/rtl/addr.sv
+++ repo/rtl/addr.sv
@@ -1,17 +1,14 @@
 module addr #(
     parameter DATA_WIDTH = 32
 )(
-    input logic [DATA_WIDTH-1:0] PC_i,
-    input logic [DATA_WIDTH-1:0] ImmOp_i,
-
-    output logic [DATA_WIDTH-1:0] branch_PC_o,
-    output logic [DATA_WIDTH-1:0] inc_PC_o
-
+    input logic [DATA_WIDTH-1:0]        PC_i,
+    input logic [DATA_WIDTH-1:0]        ImmOp_i,
+    output logic [DATA_WIDTH-1:0]       branch_PC_o,
+    output logic [DATA_WIDTH-1:0]       inc_PC_o
 );
-
     always_comb begin
         branch_PC_o = PC_i + ImmOp_i;
         inc_PC_o    = PC_i + 'd4;
     end
-    
+      
 endmodule
