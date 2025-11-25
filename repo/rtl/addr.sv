eae0c373f1d083f95fe549acafa8ec77b5b87542
Yichan Kim <yichankim@gmail.com>
2025-11-25 17:44:57 +0000

    Added suffix to  variables in addr

--- repo/rtl/addr.sv
+++ repo/rtl/addr.sv
@@ -1,17 +1,17 @@
 module addr #(
     parameter DATA_WIDTH = 32
 )(
-    input logic [DATA_WIDTH-1:0] PC_o,
-    input logic [DATA_WIDTH-1:0] ImmOp,
+    input logic [DATA_WIDTH-1:0] PC_i,
+    input logic [DATA_WIDTH-1:0] ImmOp_i,
 
-    output logic [DATA_WIDTH-1:0] branch_PC,
-    output logic [DATA_WIDTH-1:0] inc_PC
+    output logic [DATA_WIDTH-1:0] branch_PC_o,
+    output logic [DATA_WIDTH-1:0] inc_PC_o
 
 );
 
     always_comb begin
-        branch_PC = PC + ImmOp;
-        inc_PC    = PC + 'd4;
+        branch_PC_o = PC_i + ImmOp_i;
+        inc_PC_o    = PC_i + 'd4;
     end
     
 endmodule
