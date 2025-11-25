cdeecaafa2a54116293f78d4393b60a8070cadec
Yichan Kim <yichankim@gmail.com>
2025-11-25 19:16:59 +0000

    Renamed instr_mem.sv file to match lab 4 content

--- repo/rtl/instr_mem.sv
+++ repo/rtl/instr_mem.sv
@@ -2,19 +2,19 @@ module instr_mem #(
     parameter   ADDRESS_WIDTH = 32,
                 BYTE_WIDTH = 8
 )(
-    input  logic [ADDRESS_WIDTH-1:0] A_i,
-    output logic [ADDRESS_WIDTH-1:0] RD_o
+    input  logic [31:0] A_i,
+    output logic [31:0] RD_o
 );
 
-logic [ADDRESS_WIDTH-1:0] instr_rom [1023:0];
+logic [31:0] instr_rom [1023:0];
 logic [BYTE_WIDTH-1:0] byte_rom [4095:0];
 initial begin
     integer i;
-    $readmemh("../rtl/program.hex", byte_rom);
-    for (i=0; i<256; i++)
+    $readmemh("program.hex", byte_rom);
+    for (i=0; i<1024; i = i + 1)
         instr_rom[i] = {byte_rom[i*4+3], byte_rom[i*4+2], byte_rom[i*4+1], byte_rom[i*4+0]};
 end
 
-assign RD_o = instr_rom[A_i[9:2]];
+assign RD_o = instr_rom[A_i[11:2]];
 
 endmodule
