5b1a2d6ffb290bed4d3cd9ece50158ebc6dbcfa2
Yichan Kim <yichankim@gmail.com>
2025-11-25 19:14:06 +0000

    Changed variable naming, fixed suffix

--- repo/rtl/addr.sv
+++ repo/rtl/addr.sv
@@ -3,12 +3,12 @@ module addr #(
 )(
     input logic [DATA_WIDTH-1:0]        PC_i,
     input logic [DATA_WIDTH-1:0]        ImmOp_i,
-    output logic [DATA_WIDTH-1:0]       branch_PC_o,
-    output logic [DATA_WIDTH-1:0]       inc_PC_o
+    output logic [DATA_WIDTH-1:0]       pcTarget_o,
+    output logic [DATA_WIDTH-1:0]       pcPlus4_o
 );
     always_comb begin
-        branch_PC_o = PC_i + ImmOp_i;
-        inc_PC_o    = PC_i + 'd4;
+        pcTarget_o  = PC_i + ImmOp_i;
+        pcPlus4_o   = PC_i + 'd4;
     end
-      
+
 endmodule
