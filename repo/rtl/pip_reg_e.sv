720117e1b7f1f30cc8e490294635a5153fe106da
vortexisalpha <joshuaboshua250505@gmail.com>
2025-11-26 13:36:20 +0000

    added commas (bugfix)

--- repo/rtl/pip_reg_e.sv
+++ repo/rtl/pip_reg_e.sv
@@ -16,34 +16,34 @@ module pip_reg_e #( //Decode to execute stage
     output  logic                           MemWriteE_o //Execute
 
     input   logic                           JumpD_i, //Decode
-    output  logic                           JumpE_o //Execute
+    output  logic                           JumpE_o, //Execute
 
     input   logic                           BranchD_i, //Decode
-    output  logic                           BranchE_o //Execute
+    output  logic                           BranchE_o, //Execute
 
     input   logic [2:0]                     ALUControlD_i, //Decode
-    output  logic [2:0]                     ALUControlE_o //Execute
+    output  logic [2:0]                     ALUControlE_o, //Execute
 
     input   logic                           ALUSrcD_i, //Decode
-    output  logic                           ALUSrcE_o //Execute
+    output  logic                           ALUSrcE_o, //Execute
     
     input   logic [INSTRUCTION_WIDTH-1:0]   RD1D_i, //Decode
-    output  logic [INSTRUCTION_WIDTH-1:0]   RD1E_o //Execute
+    output  logic [INSTRUCTION_WIDTH-1:0]   RD1E_o, //Execute
     
     input   logic [INSTRUCTION_WIDTH-1:0]   RD2D_i, //Decode
-    output  logic [INSTRUCTION_WIDTH-1:0]   RD2E_o //Execute
+    output  logic [INSTRUCTION_WIDTH-1:0]   RD2E_o, //Execute
     
     input   logic [PC_WIDTH-1:0]            PCD_i, //Decode
-    output  logic [PC_WIDTH-1:0]            PCE_o //Execute
+    output  logic [PC_WIDTH-1:0]            PCE_o, //Execute
     
     input   logic [REGISTER_ADDR_SIZE-1:0]  RdD_i, //Decode
-    output  logic [REGISTER_ADDR_SIZE-1:0]  RdE_o //Execute
+    output  logic [REGISTER_ADDR_SIZE-1:0]  RdE_o, //Execute
     
     input   logic [INSTRUCTION_WIDTH-1:0]   ImmExtD_i, //Decode
-    output  logic [INSTRUCTION_WIDTH-1:0]   ImmExtE_o //Execute
+    output  logic [INSTRUCTION_WIDTH-1:0]   ImmExtE_o, //Execute
     
     input   logic [PC_WIDTH-1:0]            PCPlus4D_i, //Decode
-    output  logic [PC_WIDTH-1:0]            PCPlus4E_o //Execute
+    output  logic [PC_WIDTH-1:0]            PCPlus4E_o, //Execute
     
 );
 
