b8e943789a1497b667547fe45e40855a5b29d339
carys <caryshcleung@gmail.com>
2025-12-01 01:11:06 +0000

    Updated single cycle PC multiplexer

--- repo/rtl/mux_reg.sv
+++ repo/rtl/mux_reg.sv
@@ -1,11 +1,18 @@
 module mux_reg #(
-    DATA_WIDTH = 32
-) (
-    input   logic [DATA_WIDTH-1:0]  inc_PC,
-    input   logic [DATA_WIDTH-1:0]  branch_PC,
-    input   logic                   PC_src,
-    output  logic [DATA_WIDTH-1:0]  next_PC
+    parameter DATA_WIDTH = 32
+)(
+    input   logic [DATA_WIDTH-1:0]  inc_PC_i,
+    input   logic [DATA_WIDTH-1:0]  branch_PC_i,
+    input   logic [DATA_WIDTH-1:0]  jalr_PC_i,
+    input   logic [1:0]             PC_src_i,
+    output  logic [DATA_WIDTH-1:0]  next_pc_o
 );
-    assign next_PC = PC_src ? branch_PC : inc_PC;
-
+    always_comb begin
+        case (PC_src_i)
+            2'b00: next_pc_o = inc_PC_i; // next instruction
+            2'b01: next_pc_o = branch_PC_i; // branch or JAL
+            2'b10: next_pc_o = jalr_PC_i; // JALR
+            default: next_pc_o = inc_PC_i; // PC + 4
+        endcase
+    end
 endmodule
