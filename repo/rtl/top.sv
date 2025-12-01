574af08b860a52e68e17a26355949ea86eb17724
carys <caryshcleung@gmail.com>
2025-12-01 02:11:55 +0000

    Updated single cycle top module

--- repo/rtl/top.sv
+++ repo/rtl/top.sv
@@ -1,342 +1,118 @@
 module top #(
-    REGISTER_ADDRESS_WIDTH = 5,
-    DATA_WIDTH = 32,
-    INSTRUCTION_WIDTH = 32,
-    PC_WIDTH = 32
-) (
+    parameter DATA_WIDTH = 32
+)(
     input   logic                   clk,
     input   logic                   rst,
+    /* verilator lint_off UNUSED */
     input   logic                   trigger,
-    output  logic [DATA_WIDTH-1:0]  a0
+    /* verilator lint_on UNUSED */
+    output  logic [DATA_WIDTH-1:0]  a0    
 );
 
-    //mux 0
-    logic [PC_WIDTH-1:0]            PCNext;
+    logic [DATA_WIDTH-1:0]  PC;
+    logic [DATA_WIDTH-1:0]  PCNext;
+    logic [DATA_WIDTH-1:0]  PCPlus4;
+    logic [DATA_WIDTH-1:0]  branch_target;
+
+    logic [DATA_WIDTH-1:0]  Instr;
+    logic [DATA_WIDTH-1:0]  ImmExt;
+    logic [DATA_WIDTH-1:0]  SrcA;
+    logic [DATA_WIDTH-1:0]  SrcB;
+    logic [DATA_WIDTH-1:0]  ReadData2;
+    logic [DATA_WIDTH-1:0]  ALUResult;
+    logic [DATA_WIDTH-1:0]  ReadData;
+    logic [DATA_WIDTH-1:0]  Result;
+    logic                   Zero;
+    logic                   RegWrite;
+    logic                   MemWrite;
     
-    //shift register
-    logic [PC_WIDTH-1:0]            PCF; //Fetch
-    logic [PC_WIDTH-1:0]            PCE; //Execute
-    logic [PC_WIDTH-1:0]            PCD; //Decode
-    
-    //Instruction Memory Outputs
-    logic[INSTRUCTION_WIDTH-1:0]    InstrF; //Fetch
-    logic[INSTRUCTION_WIDTH-1:0]    InstrD; //Decode
+    logic                   ALUSrc;
+    logic [1:0]             PCSrc;
+    logic [1:0]             ResultSrc;
+    logic [2:0]             ALUControl;
+    logic [2:0]             ImmSrc;
 
-    //PC adder
-    logic [PC_WIDTH-1:0]            PCPlus4F; //Fetch
-    logic [PC_WIDTH-1:0]            PCPlus4D; //Decode
-    logic [PC_WIDTH-1:0]            PCPlus4E; //Decode
-    logic [PC_WIDTH-1:0]            PCPlus4M; //Memory
-    logic [PC_WIDTH-1:0]            PCPlus4W; //Writeback
+    addr PC_arithmetic (
+        .PC_i(PC), 
+        .ImmOp_i(ImmExt), 
+        .branch_PC_o(branch_target), 
+        .inc_PC_o(PCPlus4)
+    );
 
+    mux_reg PC_mux (
+        .inc_PC_i(PCPlus4), 
+        .branch_PC_i(branch_target), 
+        .jalr_PC_i(ALUResult), 
+        .PC_src_i(PCSrc),
+        .next_pc_o(PCNext)
+    );
 
-    //Control block outputs
-<<<<<<< HEAD
-    logic                           PCSrcE;
-    logic                           RegWriteD;
-    logic                           RegWriteE;
-    logic                           RegWriteM;
-    logic                           RegWriteW;
-    logic [1:0]                     ResultSrcD;
-    logic [1:0]                     ResultSrcE;
-    logic [1:0]                     ResultSrcM;
-    logic [1:0]                     ResultSrcW;
-    logic                           MemWriteD;
-    logic                           MemWriteE;
-    logic                           MemWriteM;
-    logic                           JumpD;
-    logic                           JumpE;
-    logic                           BranchD;
-    logic                           BranchE;
-    logic [2:0]                     ALUControlD;
-    logic [2:0]                     ALUControlE;
-    logic                           ALUSrcD;
-    logic                           ALUSrcE;
-    logic [2:0]                     ImmSrcD;
-
-=======
-    logic                           PCSrc;
-    logic                           ResultSrc;
-    logic                           MemWrite;
-    logic [2:0]                     ALUControl;
-    logic                           ALUSrc;
-    logic [2:0]                     ImmSrc;
-    logic                           RegWrite;
->>>>>>> d66f952e939157a6850dcd2adfbc1be02cb4cd4c
-
-    //Register File Outputs
-    logic[DATA_WIDTH-1:0]           RD1D;
-    logic[DATA_WIDTH-1:0]           RD1E;
-    logic[DATA_WIDTH-1:0]           RD2D;
-    logic[DATA_WIDTH-1:0]           RD2E;
-
-
-    //Extend Output
-    logic[DATA_WIDTH-1:0]           ImmExtD;
-    logic[DATA_WIDTH-1:0]           ImmExtE;
-
-
-    //ALU Input Wires
-    logic[DATA_WIDTH-1:0]           WriteDataE;
-    logic[DATA_WIDTH-1:0]           WriteDataM;
-    logic[DATA_WIDTH-1:0]           SrcAE;
-    logic[DATA_WIDTH-1:0]           SrcBE;
-   
-    //ALU Output
-    logic[DATA_WIDTH-1:0]           ALUResultE; //Execute
-    logic[DATA_WIDTH-1:0]           ALUResultM; //Memory
-    logic[DATA_WIDTH-1:0]           ALUResultW; //Writeback
-    logic                           ZeroE;
-
-    //Adder
-    logic [PC_WIDTH-1:0]            PCTargetE;
-    
-    //Data Memory
-    logic[DATA_WIDTH-1:0]           ReadDataM; //Memory
-    logic[DATA_WIDTH-1:0]           ReadDataW; //Writeback
-
-    //Mux 2
-    logic[DATA_WIDTH-1:0]           ResultW;
-
-    //Hazard Unit
-    logic                           StallF;
-    logic                           StallD;
-    logic                           FlushD;
-    logic                           FlushE;
-    logic[1:0]                      ForwardAE;
-    logic[1:0]                      ForwardBE;
-
-    //Control block inputs 
-    logic [6:0]                     op;
-    logic [2:0]                     funct3;
-    logic                           funct7;
-
-    assign PCSrcE = JumpE || (BranchE && ZeroE);
-    assign PCNext = PCSrcE ? PCTargetE : PCPlus4F;
-    // NOTE: after control block changes PCSrcE, make PCSrcE = the or stuff in diag
-
-    pc_reg pc_reg (
+    pc_reg program_counter (
         .clk_i(clk),
         .rst_i(rst),
-        .PCNext_i(PCNext),
-        .en_i(!StallF), //from Hazard Unit
-        .PC_o(PCF)
+        .next_pc_i(PCNext),
+        .PC_o(PC)
     );
 
-    hazard_unit hazard_unit(
-        .Rs1D_i(Rs1D),
-        .Rs2D_i(Rs2D),
-        .Rs1E_i(Rs1E),
-        .Rs2E_i(Rs2E),
-        .RdE_i(RdE),
-        .ResultSrcE0_i(ResultSrcE[0]),
-        .RdM_i(RdM),
-        .RegWriteM_i(RegWriteM),
-        .RdW_i(RdW),
-        .RegWriteW_i(RegWriteW),
-        .PCSrcE_i(PCSrcE),
-        .ForwardAE_o(ForwardAE),
-        .ForwardBE_o(ForwardBE),
-        .StallF_o(StallF),
-        .StallD_o(StallD),
-        .FlushD_o(FlushD),
-        .FlushE_o(FlushE)
+    instr_mem Instruction_Memory (
+        .A_i(PC),
+        .RD_o(Instr)
     );
 
-    instr_mem instr_mem (
-        .A_i(PCF),
-        .RD_o(InstrF)
-    );
-
-    addr addr(
-        .PCF_i(PCF),
-        .PCE_i(PCE),
-        .ImmOp_i(ImmExtE),
-        .PCTarget_o(PCTargetE),
-        .PCPlus4_o(PCPlus4F)
-    );
-
-    pip_reg_d pip_reg_d (
-        .clk_i(clk),
-        .en_i(!StallD),
-        .clr_i(FlushD),
-        .PCF_i(PCF),
-        .InstrF_i(InstrF),
-        .PCPlus4F_i(PCPlus4F),
-        .PCD_o(PCD),
-        .InstrD_o(InstrD),
-        .PCPlus4D_o(PCPlus4D)
-    );
-
-    assign op = InstrD[6:0];
-    assign funct3 = InstrD[14:12];
-    assign funct7 = InstrD[30];
-
-
-    logic [REGISTER_ADDRESS_WIDTH-1:0] Rs1D;
-    logic [REGISTER_ADDRESS_WIDTH-1:0] Rs1E;
-    logic [REGISTER_ADDRESS_WIDTH-1:0] Rs2D;
-    logic [REGISTER_ADDRESS_WIDTH-1:0] Rs2E;
-    logic [REGISTER_ADDRESS_WIDTH-1:0] RdD;
-    logic [REGISTER_ADDRESS_WIDTH-1:0] RdE;
-    logic [REGISTER_ADDRESS_WIDTH-1:0] RdM;
-    logic [REGISTER_ADDRESS_WIDTH-1:0] RdW;
-
-
-    assign Rs1D = InstrD[19:15];
-    assign Rs2D = InstrD[24:20];
-    assign RdD = InstrD[11:7];
-
-
-    //Control unit needs to be modified to have JumpD, BranchD
-    //Control unit must not take input from zeroE, get rid of PCSrcE output pls
-    control_unit control_unit(
-        .op_i(op),
-        .Zero_i(ZeroE),
-        .funct3_i(funct3),
-        .funct7_i(funct7),
-<<<<<<< HEAD
-        .RegWrite_o(RegWriteD),
-        .MemWrite_o(MemWriteD),
-        .ALUControl_o(ALUControlD),
-        .ALUSrc_o(ALUSrcD),
-        .ImmSrc_o(ImmSrcD),
-        .ResultSrc_o(ResultSrcD),
-        .PCSrc_o(JumpD) // CHANGE CU TO OUTPUT JUMP, BRANCH STUFF INSTEAD 
-        // SHOULD ALSO HAVE A BRANCHD OUTPUT HERE
-=======
+    control_unit control_unit (
+        .op_i(Instr[6:0]),
+        .funct3_i(Instr[14:12]),
+        .funct7_i(Instr[30]), 
+        .Zero_i(Zero),
         .RegWrite_o(RegWrite),
-        .MemWrite_o(MemWrite),
+        .MemWrite_o(MemWrite), 
         .ALUControl_o(ALUControl),
-        .ResultSrc_o(ResultSrc),
         .ALUSrc_o(ALUSrc),
-        .ImmSrc_o(ImmSrc),
+        .ImmSrc_o(ImmSrc), 
+        .ResultSrc_o(ResultSrc),
         .PCSrc_o(PCSrc)
->>>>>>> d66f952e939157a6850dcd2adfbc1be02cb4cd4c
     );
 
-    //variable changing is needed
-    sign_extend sign_extend (
-<<<<<<< HEAD
-        .imm_src_i(ImmSrcD),
-        .imm_instr_i(InstrD), //input
-        .imm_ext_o(ImmExtD)
-=======
-        .ImmSrc_i(ImmSrc),
-        .ImmInstr_i(Instr), //input
-        .ImmExt_o(ImmExt)
->>>>>>> d66f952e939157a6850dcd2adfbc1be02cb4cd4c
-    );
-    
-
-    regfile regfile(
-        .clk_i(!clk),
-        .A1_i(Rs1D),
-        .A2_i(Rs2D),
-        .A3_i(RdW),
-        .WD3_i(ResultW),
-        .WE3_i(RegWriteW),
-        .RD1_o(RD1D),
-        .RD2_o(RD2D),
-        .A0_o(a0) 
-    );
-
-
-    pip_reg_e pip_reg_e(
+    regfile register_file (
         .clk_i(clk),
-        .clr_i(FlushE),
-        .RegWriteD_i(RegWriteD),
-        .RegWriteE_o(RegWriteE),
-        .ResultSrcD_i(ResultSrcD),
-        .ResultSrcE_o(ResultSrcE),
-        .MemWriteD_i(MemWriteD),
-        .MemWriteE_o(MemWriteE),
-        .JumpD_i(JumpD),
-        .JumpE_o(JumpE),
-        .BranchD_i(BranchD),
-        .BranchE_o(BranchE),
-        .ALUControlD_i(ALUControlD),
-        .ALUControlE_o(ALUControlE),
-        .ALUSrcD_i(ALUSrcD),
-        .ALUSrcE_o(ALUSrcE),
-        .RD1D_i(RD1D),
-        .RD1E_o(RD1E),
-        .RD2D_i(RD2D),
-        .RD2E_o(RD2E),
-        .PCD_i(PCD),
-        .PCE_o(PCE),
-        .Rs1D_i(Rs1D),
-        .Rs1E_o(Rs1E),
-        .Rs2D_i(Rs2D),
-        .Rs2E_o(Rs2E),
-        .RdD_i(RdD),
-        .RdE_o(RdE),
-        .ImmExtD_i(ImmExtD),
-        .ImmExtE_o(ImmExtE),
-        .PCPlus4D_i(PCPlus4D),
-        .PCPlus4E_o(PCPlus4E)
+        .A1_i(Instr[19:15]),
+        .A2_i(Instr[24:20]),
+        .A3_i(Instr[11:7]),  
+        .WD3_i(Result),
+        .WE3_i(RegWrite),
+        .RD1_o(SrcA),
+        .RD2_o(ReadData2),
+        .A0_o(a0)            
     );
 
-    //3way mux so assumes ForwardAE != 11
-    assign SrcAE = ForwardAE[1] ? (ForwardAE[0] ? 'b0 : ALUResultM) : (ForwardAE[0] ? ResultW : RD1E); 
-    //3way mux so assumes ForwardBE != 11
-    assign WriteDataE = ForwardBE[1] ? (ForwardBE[0] ? 'b0 : ALUResultM) : (ForwardBE[0] ? ResultW : RD2E);
-    assign SrcBE = ALUSrcE ? ImmExtE : WriteDataE; 
+    sign_extend sign_extend (
+        .ImmSrc_i(ImmSrc),
+        .ImmInstr_i(Instr),
+        .ImmExt_o(ImmExt)
+    );
+
+    // ALU mux
+    assign SrcB = ALUSrc ? ImmExt : ReadData2;
 
     ALU ALU (
-        .SrcA_i(SrcAE),
-        .SrcB_i(SrcBE),
-        .ALUControl_i(ALUControlE),
-        .ALUResult_o(ALUResultE),
-        .Zero_o(ZeroE)
-    );    
-
-
-    pip_reg_m pip_reg_m(
-        .clk_i(clk),
-        .RegWriteE_i(RegWriteE),
-        .RegWriteM_o(RegWriteM),
-        .ResultSrcE_i(ResultSrcE),
-        .ResultSrcM_o(ResultSrcM),
-        .MemWriteE_i(MemWriteE),
-        .MemWriteM_o(MemWriteM),
-        .ALUResultE_i(ALUResultE),
-        .ALUResultM_o(ALUResultM),
-        .WriteDataE_i(WriteDataE), 
-        .WriteDataM_o(WriteDataM), 
-        .RdE_i(RdE),
-        .RdM_o(RdM),
-        .PCPlus4E_i(PCPlus4E),
-        .PCPlus4M_o(PCPlus4M)
+        .SrcA_i(SrcA),
+        .SrcB_i(SrcB),
+        .ALUControl_i(ALUControl), 
+        .ALUResult_o(ALUResult),
+        .Zero_o(Zero)
     );
 
-    data_memory data_memory(
-        .clk_i(clk),
-        .wr_en_i(MemWriteM),
-        .addr_i(ALUResultM),
-        .data_i(WriteDataM),
-        .data_o(ReadDataM)
+    data_memory data_memory (
+        .clk_i(clk), 
+        .wr_en_i(MemWrite), 
+        .funct3_i(Instr[14:12]),
+        .addr_i(ALUResult), 
+        .data_i(ReadData2), 
+        .data_o(ReadData)
     );
 
-    pip_reg_w pip_reg_w(
-        .clk_i(clk),
-        .RegWriteM_i(RegWriteM),
-        .RegWriteW_o(RegWriteW),
-        .ResultSrcM_i(ResultSrcM),
-        .ResultSrcW_o(ResultSrcW),
-        .ALUResultM_i(ALUResultM),
-        .ALUResultW_o(ALUResultW),
-        .ReadDataM_i(ReadDataM),
-        .ReadDataW_o(ReadDataW),
-        .RdM_i(RdM),
-        .RdW_o(RdW),
-        .PCPlus4M_i(PCPlus4M),
-        .PCPlus4W_o(PCPlus4W)
-    );
-
-    //mux 3
-
-    assign ResultW = ResultSrcW[1] ? (ResultSrcW[0] ? 'b0 : PCPlus4W) : (ResultSrcW[0] ? ReadDataW : ALUResultW);
-
+    // writeback mux
+    assign Result = (ResultSrc == 2'b01) ? ReadData : 
+                    (ResultSrc == 2'b10) ? PCPlus4  : ALUResult;
 
 endmodule
