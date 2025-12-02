496b67961666946dd1931d7c6c3923bd67abc485
carys <caryshcleung@gmail.com>
2025-12-02 23:29:27 +0000

    Added new branch unit for execution stage

---
+++ repo/rtl/branch_unit.sv
@@ -0,0 +1,20 @@
+module branch_unit #(
+    parameter DATA_WIDTH = 32
+)(
+    input  logic [2:0]            funct3_i,
+    input  logic                  Zero_i,
+    input  logic [DATA_WIDTH-1:0] ALUResult_i,
+    output logic                  BranchTaken_o
+);
+
+    always_comb begin
+        case(funct3_i)
+            3'b000: BranchTaken_o = Zero_i;          // BEQ
+            3'b001: BranchTaken_o = !Zero_i;         // BNE
+            3'b100: BranchTaken_o = ALUResult_i[31]; // BLT
+            3'b101: BranchTaken_o = !ALUResult_i[31];// BGE
+            default: BranchTaken_o = 1'b0;
+        endcase
+    end
+
+endmodule
