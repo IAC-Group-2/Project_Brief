f68281c6742a50c3a7f018799b260cfa654498b1
vortexisalpha <joshuaboshua250505@gmail.com>
2025-12-05 15:17:12 +0000

    created full cache implementation, dealt with FSM for cache miss states and stalling. CO-AUTHORED: Yichan (live video call)

--- repo/rtl/cache.sv
+++ repo/rtl/cache.sv
@@ -5,9 +5,9 @@ module cache #(
                 BYTE_WIDTH = 8,
                 SET_SIZE = 8,
                 TAG_SIZE = 22,
-                CACHE_WIDTH = 123,
                 BYTE_OFFSET_WIDTH = 2,
-                WAY_WIDTH = 55
+                WAY_WIDTH = 56,             // V(1) + Dirty(1) + Tag(22) + Data(32) = 56
+                CACHE_WIDTH = 113           // U(1) + Way0(56) + Way1(56) = 113
 
 )(
     input   logic                       clk_i,
@@ -17,7 +17,13 @@ module cache #(
     input   logic [1:0]                 ResultSrcM_i,   //Read enable when ResultSrcM_i = 2'b01
 
     input   logic [ADDRESS_WIDTH-1:0]   addr_i,         //input address
-    input   logic [DATA_WIDTH-1:0]      data_i,         //input data 
+    input   logic [DATA_WIDTH-1:0]      data_i,         //input data
+
+    //memory interface
+    input   logic [DATA_WIDTH-1:0]      mem_rd_data_i,  //data from main memory
+    output  logic [ADDRESS_WIDTH-1:0]   mem_addr_o,     //address to main memory
+    output  logic                       mem_wr_en_o,    //write enable to main memory
+    output  logic [DATA_WIDTH-1:0]      mem_wr_data_o,  //data to write to main memory **rename
 
     output  logic [DATA_WIDTH-1:0]      data_o,         //output data
     output  logic                       cache_miss_o,   //cache_miss = 1 if didnt find in cache
@@ -29,10 +35,12 @@ module cache #(
     assign rd_en = ResultSrcM_i == 2'b01;
     logic cache_en;
     assign cache_en = (rd_en || wr_en);
-    
 
+    //state machine states
+    typedef enum {IDLE, WRITEBACK, FETCH, UPDATE} my_state;
+    my_state current_state, next_state;
     
-    // U | V1 | Tag1(22 bits) | Data 1 (32 bits) | V2 | Tag2 (22 bits) | Data 2 (32 bits)
+    // U | V1 | Dirty1 | Tag1(22 bits) | Data 1 (32 bits) | V2 | Dirty2 | Tag2 (22 bits) | Data 2 (32 bits)
     logic [CACHE_WIDTH-1:0] cache_array [2**SET_SIZE-1:0];
     
     logic [SET_SIZE-1:0] set_addr;
@@ -43,6 +51,8 @@ module cache #(
 
     logic cache_valid_0;
     logic cache_valid_1;
+    logic cache_dirty_0;
+    logic cache_dirty_1;
 
     logic [WAY_WIDTH-1:0] cache_way_0;
     logic [WAY_WIDTH-1:0] cache_way_1;
@@ -53,7 +63,6 @@ module cache #(
     logic [DATA_WIDTH-1:0] cache_data_0;
     logic [DATA_WIDTH-1:0] cache_data_1;
     
-    
     logic [CACHE_WIDTH-1:0] cache_set;
     logic lru_bit;
 
@@ -63,18 +72,23 @@ module cache #(
     assign lru_bit = cache_set[CACHE_WIDTH-1];
 
     assign cache_valid_0 = cache_way_0[WAY_WIDTH-1];
-    assign cache_valid_1 = cache_way_1[WAY_WIDTH-1];
-
-    assign cache_tag_0 = cache_way_0[WAY_WIDTH-2:DATA_WIDTH];
-    assign cache_tag_1 = cache_way_1[WAY_WIDTH-2:DATA_WIDTH];
-
+    assign cache_dirty_0 = cache_way_0[WAY_WIDTH-2];
+    assign cache_tag_0 = cache_way_0[WAY_WIDTH-3:DATA_WIDTH];
     assign cache_data_0 = cache_way_0[DATA_WIDTH-1:0];
+
+    assign cache_valid_1 = cache_way_1[WAY_WIDTH-1];
+    assign cache_dirty_1 = cache_way_1[WAY_WIDTH-2];
+    assign cache_tag_1 = cache_way_1[WAY_WIDTH-3:DATA_WIDTH];
     assign cache_data_1 = cache_way_1[DATA_WIDTH-1:0];
 
     logic tag_0_hit;
     logic tag_1_hit;
     logic cache_miss;
     logic cache_hit;
+    logic target_way;       //which way to replace (from LRU)
+    logic target_dirty;     //is target dirty?
+    logic [TAG_SIZE-1:0] target_tag;
+    logic [DATA_WIDTH-1:0] target_data;
 
     //hit or miss detection
     always_comb begin
@@ -84,47 +98,149 @@ module cache #(
         cache_miss = cache_en && !cache_hit;
     end
 
-    //output data on cache hit
+    //target selection based on LRU (lru_bit=0: replace way0, lru_bit=1: replace way1)
     always_comb begin
-        if (cache_hit) begin
-            data_o = tag_1_hit ? cache_data_1 : cache_data_0;
+        target_way = lru_bit;
+        if (lru_bit == 1'b0) begin
+            if (cache_valid_0) begin
+                target_dirty = cache_dirty_0;
+            end
+            else begin
+                target_dirty = 0;
+            end
+            target_tag = cache_tag_0;
+            target_data = cache_data_0;
         end else begin
-            data_o = 32'b0; //default val on miss
+            if (cache_valid_1) begin
+                target_dirty = cache_dirty_1;
+            end
+            else begin
+                target_dirty = 0;
+            end
+            target_tag = cache_tag_1;
+            target_data = cache_data_1;
         end
     end
 
-    //stall if cache miss
-    assign stall_o = cache_miss;
+    //output data
+    always_comb begin
+        if (current_state == IDLE && cache_hit) begin
+            data_o = tag_1_hit ? cache_data_1 : cache_data_0;
+        end else if (current_state == UPDATE) begin
+            data_o = mem_rd_data_i;
+        end else begin
+            data_o = 32'b0;
+        end
+    end
+
+    //memory interface outputs
+    always_comb begin
+        mem_addr_o = addr_i;
+        mem_wr_en_o = 1'b0;
+        mem_wr_data_o = 32'b0;
+        
+        case (current_state)
+            WRITEBACK: begin
+                //writeback dirty target to memory
+                mem_addr_o = {target_tag, set_addr, {BYTE_OFFSET_WIDTH{1'b0}}};
+                mem_wr_en_o = 1'b1;
+                mem_wr_data_o = target_data;
+            end
+            FETCH: begin
+                //fetch new data from memory
+                mem_addr_o = addr_i;
+                mem_wr_en_o = 1'b0;
+            end
+            default: begin
+                mem_addr_o = addr_i;
+                mem_wr_en_o = 1'b0;
+            end
+        endcase
+    end
+
+    //stall when not in IDLE or when theres a miss in IDLE
+    assign stall_o = (current_state != IDLE) || (current_state == IDLE && cache_miss);
     assign cache_miss_o = cache_miss;
+
+    //state machine next state logic
+    always_comb begin
+        case (current_state)
+            IDLE: begin
+                if (cache_miss) begin
+                    if (target_dirty)
+                        next_state = WRITEBACK;
+                    else
+                        next_state = FETCH;
+                end
+            end
+            WRITEBACK: begin
+                next_state = FETCH;
+            end
+            FETCH: begin
+                next_state = UPDATE;
+            end
+            UPDATE: begin
+                next_state = IDLE;
+            end
+            default:
+                next_state = current_state;
+        endcase
+    end
 
     //cache update logic
     always_ff @(posedge clk_i) begin
         if (rst_i) begin
-            //on reset set everything to 0
+            current_state <= IDLE;
+
             for (int i = 0; i < 2**SET_SIZE; i++) begin
-                cache_array[i] <= '0;
+                cache_array[i] = '0;
             end
-        end else if (cache_en) begin
-            if (wr_en && cache_hit) begin
-                //write hit update data and set LRU (already set data out)
-                if (tag_0_hit) begin
-                    cache_array[set_addr][DATA_WIDTH-1:0] <= data_i;
-                    cache_array[set_addr][CACHE_WIDTH-1] <= 1'b1;
-                end else if (tag_1_hit) begin
-                    cache_array[set_addr][WAY_WIDTH + DATA_WIDTH - 1 : WAY_WIDTH] <= data_i;
-                    cache_array[set_addr][CACHE_WIDTH-1] <= 1'b0;
+        end else begin
+            current_state <= next_state;
+
+            case (current_state)
+                IDLE: begin
+                    if (cache_en && cache_hit) begin
+                        if (wr_en) begin
+                            //write hit update data and set LRU and dirty bit (already set data out)
+                            if (tag_0_hit) begin
+                                cache_array[set_addr][DATA_WIDTH-1:0] <= data_i;
+                                cache_array[set_addr][DATA_WIDTH + TAG_SIZE] <= 1'b1;
+                                cache_array[set_addr][CACHE_WIDTH-1] <= 1'b1;
+                            end else if (tag_1_hit) begin
+                                cache_array[set_addr][WAY_WIDTH + DATA_WIDTH - 1 : WAY_WIDTH] <= data_i;
+                                cache_array[set_addr][WAY_WIDTH + DATA_WIDTH + TAG_SIZE] <= 1'b1;
+                                cache_array[set_addr][CACHE_WIDTH-1] <= 1'b0;
+                            end
+                        end else if (rd_en) begin
+                            //read hit: update LRU only
+                            if (tag_0_hit)
+                                cache_array[set_addr][CACHE_WIDTH-1] <= 1'b1;
+                            else if (tag_1_hit)
+                                cache_array[set_addr][CACHE_WIDTH-1] <= 1'b0;
+                        end
+                    end
                 end
-            end else if (rd_en && cache_hit) begin
-                //read data set LRU (already set data out)
-                if (tag_0_hit) begin
-                    cache_array[set_addr][CACHE_WIDTH-1] <= 1'b1;
-                end else if (tag_1_hit) begin
-                    cache_array[set_addr][CACHE_WIDTH-1] <= 1'b0;
+                UPDATE: begin
+                    //fill cache with fetched data
+                    if (target_way == 1'b0) begin
+                        //update way 0: {valid, dirty, tag, data}
+                        cache_array[set_addr][DATA_WIDTH-1:0] <= wr_en ? data_i : mem_rd_data_i; //data
+                        cache_array[set_addr][WAY_WIDTH-3:DATA_WIDTH] <= tag_addr; //tag
+                        cache_array[set_addr][DATA_WIDTH + TAG_SIZE] <= wr_en ? 1'b1 : 1'b0; //dirty
+                        cache_array[set_addr][WAY_WIDTH-1] <= 1'b1; //valid
+                        cache_array[set_addr][CACHE_WIDTH-1] <= 1'b1; //LRU
+                    end else begin
+                        //update way 1
+                        cache_array[set_addr][WAY_WIDTH + DATA_WIDTH - 1 : WAY_WIDTH] <= wr_en ? data_i : mem_rd_data_i; //data
+                        cache_array[set_addr][WAY_WIDTH + WAY_WIDTH - 3 : WAY_WIDTH + DATA_WIDTH] <= tag_addr; //tag
+                        cache_array[set_addr][WAY_WIDTH + DATA_WIDTH + TAG_SIZE] <= wr_en ? 1'b1 : 1'b0; //dirty
+                        cache_array[set_addr][2*WAY_WIDTH-1] <= 1'b1; //valid
+                        cache_array[set_addr][CACHE_WIDTH-1] <= 1'b0; //LRU
+                    end
                 end
-            end
-            // on cache miss, the data will be loaded from main memory
-            // this part will be implemented when connecting to data_memory
-            // for now, the stall signal will tell the pipeline to wait
+                default: ;//ignore
+            endcase
         end
     end
 
